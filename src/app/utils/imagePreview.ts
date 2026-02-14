c79ea90f39b78703a96e72a1766e4910c32db117
shaharul siyam <shaharulsiyam@shaharuls-Mac-mini.local>
2026-02-14 14:52:22 +0600

    update

--- src/app/utils/imagePreview.ts
+++ src/app/utils/imagePreview.ts
@@ -1,15 +1,15 @@
-export const url: string = 'https://api.divandioneapp.com'
+export const url: string = 'https://api.divandione.com'
 // export const url: string = 'http://10.10.20.54:5000'
 
 // export const url: string = 'http://localhost:5000'
 // export const url: string = 'http://localhost:5000'
 
 export const imageUrl = ({ image }: { image: string }) => {
-    if (!image) return "https://placehold.co/400"
-    const fixedImage = image.replace(/\\/g, "/")
-    return fixedImage.startsWith("http")
-        ? fixedImage
-        : fixedImage.startsWith("/")
-            ? `${url}${fixedImage}`
-            : `${url}/${fixedImage}`
+  if (!image) return "https://placehold.co/400"
+  const fixedImage = image.replace(/\\/g, "/")
+  return fixedImage.startsWith("http")
+    ? fixedImage
+    : fixedImage.startsWith("/")
+      ? `${url}${fixedImage}`
+      : `${url}/${fixedImage}`
 }
