649372c9c7bf71169eaef4d58cde38f637efddeb
manik <maniksarker652@gmail.com>
2026-02-28 11:54:18 +0600

    s

--- src/app/utils/imagePreview.ts
+++ src/app/utils/imagePreview.ts
@@ -1,15 +1,14 @@
-export const url: string = 'https://api.divandione.com'
-// export const url: string = 'http://10.10.20.54:5000'
+// export const url: string = 'https://api.divandione.com'
+export const url: string = 'http://23.21.215.96:5000';
 
-// export const url: string = 'http://localhost:5000'
 // export const url: string = 'http://localhost:5000'
 
 export const imageUrl = ({ image }: { image: string }) => {
-  if (!image) return "https://placehold.co/400"
-  const fixedImage = image.replace(/\\/g, "/")
-  return fixedImage.startsWith("http")
+  if (!image) return 'https://placehold.co/400';
+  const fixedImage = image.replace(/\\/g, '/');
+  return fixedImage.startsWith('http')
     ? fixedImage
-    : fixedImage.startsWith("/")
+    : fixedImage.startsWith('/')
       ? `${url}${fixedImage}`
-      : `${url}/${fixedImage}`
-}
+      : `${url}/${fixedImage}`;
+};
